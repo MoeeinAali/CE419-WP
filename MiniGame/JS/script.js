4a9f31e93a328d86fbfe356d63e0d2c5f8447dca
Moeein Aali <moeeeinaali@gmail.com>
2025-11-02 11:12:58 +0330

    M@N 1n SANGoDooost N4daram

---
+++ MiniGame/JS/script.js
@@ -0,0 +1,145 @@
+var $;
+var CC;
+$ = function (c, i) {
+  setTimeout(function () {
+    console.log(c);
+  }, (i * (1 << (3 + "0" + 0 * 10 - 1))) / !undefined);
+};
+var a = +[];
+var b = a + ! +[];
+var c = a + b + b;
+var D = c + b;
+var E = D + b;
+var f = E + b;
+var g = f + b;
+var H = g + b;
+var I = H + b;
+var j = I + b;
+var K = j + b;
+var l = K + b;
+var m = l + b;
+var P = m + b;
+var q = P + b;
+var r = q + b;
+var s = r + b;
+var T = s + b;
+var u = T + b;
+var v = u + b;
+var w = v + b;
+var X = w + b;
+var Y = X + b;
+var Z = Y + b;
+var AA = Z + b;
+var BB = AA + b;
+CC = BB + b;
+var V1 = function () {
+  $(" ", v);
+};
+V2 = function () {
+  $("1", E);
+};
+var V3 = function () {
+  $("o", r);
+};
+var V4 = function () {
+  $("m", CC);
+};
+V5 = function () {
+  $("@", b);
+};
+var V6 = function () {
+  $(" ", m);
+};
+var V7 = function () {
+  $("a", Z);
+};
+V8 = function () {
+  $("D", P);
+};
+var V9 = function () {
+  $("M", a);
+};
+var V10 = function () {
+  $("s", T);
+};
+V11 = function () {
+  $("n", f);
+};
+var V12 = function () {
+  $("N", c);
+};
+var V13 = function () {
+  $("d", Y);
+};
+var V14 = function () {
+  $("G", K);
+};
+var V15 = function () {
+  $("r", AA);
+};
+var V16 = function () {
+  $(" ", g);
+};
+var V17 = function () {
+  $("a", BB);
+};
+var V18 = function () {
+  $("o", l);
+};
+var V19 = function () {
+  $(" ", D);
+};
+var V20 = function () {
+  $("A", I);
+};
+var V21 = function () {
+  $("4", X);
+};
+V22 = function () {
+  $("o", s);
+};
+var V23 = function () {
+  $("S", H);
+};
+var V24 = function () {
+  $("N", w);
+};
+var V25 = function () {
+  $("t", u);
+};
+var V26 = function () {
+  $("N", j);
+};
+var V27 = function () {
+  $("o", q);
+};
+V1();
+V2();
+V3();
+V4();
+V5();
+V6();
+V7();
+V8();
+V9();
+V10();
+V11();
+V12();
+V13();
+V14();
+V15();
+V16();
+V17();
+V18();
+V19();
+V20();
+V21();
+V22();
+V23();
+V24();
+V25();
+V26();
+V27();
+
+// Result = M@N 1n SANGoDooost N4daram
+
